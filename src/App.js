f355bf6ba51be935e4c0595df15ff3b84b318550
eg9622 <dlwldnjs9622@naver.com>
2019-04-26 15:43:28 +0900

    'loading'

--- src/App.js
+++ src/App.js
@@ -5,51 +5,52 @@ import Movie from './Movie';
 
 class App extends Component{
 
-  state={
-    movies:[
-        {
-          title: '타이타닉',
-          poster:'http://image.cine21.com/resize/cine21/poster/2018/0102/17_50_39__5a4b47df43cd9[X230,330].jpg'
-        },
-        {
-          title:'극한직업',
-          poster:'http://image.cine21.com/resize/cine21/poster/2019/0129/14_35_36__5c4fe6280c438[X230,330].jpg'
-        },
-        {
-          title:'컨저링',
-          poster:'http://image.cine21.com/resize/cine21/poster/2013/0902/10_10_02__5223e56a44558[X230,330].jpg'
-        },
-        {
-          title:'도둑들',
-          poster:'http://image.cine21.com/resize/cine21/poster/2012/0629/09_53_58__4fecfca6a36e3[X230,330].jpg'
-        }
-    ]
-  }
+  state={}
 
   //5초뒤에 새로운 영화가 추가되기
   componentDidMount(){
     setTimeout(()=>{
       this.setState({
         movies:[
-          ...this.state.movies,
           {
-            title:'스파이더맨',
-            poster:'http://image.cine21.com/resize/IMGDB/still/2004/0524/large/142849_01[X230,330].jpg'
+            title: '타이타닉',
+            poster:'http://image.cine21.com/resize/cine21/poster/2018/0102/17_50_39__5a4b47df43cd9[X230,330].jpg'
+          },
+          {
+            title:'극한직업',
+            poster:'http://image.cine21.com/resize/cine21/poster/2019/0129/14_35_36__5c4fe6280c438[X230,330].jpg'
+          },
+          {
+            title:'컨저링',
+            poster:'http://image.cine21.com/resize/cine21/poster/2013/0902/10_10_02__5223e56a44558[X230,330].jpg'
+          },
+          {
+            title:'도둑들',
+            poster:'http://image.cine21.com/resize/cine21/poster/2012/0629/09_53_58__4fecfca6a36e3[X230,330].jpg'
           }
-          
-        ]
+      ]
       })
-    },5000)
+    },2000)
   }
   
+  //랜더링 function
+  _renderMovies =() =>{
+    const movies=this.state.movies.map((x,index)=>{
+      return <Movie title={x.title} poster={x.poster} key={index}/>
+    });
+    return movies;
+  }
+
+
   render(){
     return (
       <div className = "App">
      
       {
-        this.state.movies.map((x,index)=>{
-          return <Movie title={x.title} poster={x.poster} key={index}/>
-        })
+        //this.state.movies가 있을땐 _renderMovies 실행
+        //state가 비어있는 상황일땐 loading...문구
+        this.state.movies ? this._renderMovies() : 'Loading'
+        
       }
       </div>
     );
