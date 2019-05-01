2d3d14a85757a82795552574bd45931ba173ef65
eg9622 <dlwldnjs9622@naver.com>
2019-05-01 16:08:28 +0900

    'movieApp'

--- src/App.js
+++ src/App.js
@@ -6,37 +6,46 @@ import Movie from './Movie';
 class App extends Component{
 
   state={}
+  /*
+  async, await
+   fetch로 받아온 데이터와 그걸 처리하는 then들을 더욱 간결하고 명료하게 만들어주는 도구
+   fetch와 then을 사용해서 일일이 모든 동작을 지정해주지 않고,
+   fetch가 끝난 다음 바로 실행시킬 것들을 지정할 수 있게 만드는 tool
+
+   async: 이전라인의 작업이 끝날때까지 기다리징 낳고 실행 될 작업
+   await: 해당 기능이 끝나는 것을 기다리고, 바로 실행
+
+   fetch(주소).then(a => a.json()).then(b => b...).then(...)
+   */
+  _callApi = () =>{
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(b => b.json())
+    .then(c => c.data.movies)
+    .catch(err => console.log(err));  //err가 났을때 catch
+  }
+
+  _getMovies = async () =>{
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  }
 
   //5초뒤에 새로운 영화가 추가되기
-  componentDidMount(){
-    setTimeout(()=>{
-      this.setState({
-        movies:[
-          {
-            title: '타이타닉',
-            poster:'http://image.cine21.com/resize/cine21/poster/2018/0102/17_50_39__5a4b47df43cd9[X230,330].jpg'
-          },
-          {
-            title:'극한직업',
-            poster:'http://image.cine21.com/resize/cine21/poster/2019/0129/14_35_36__5c4fe6280c438[X230,330].jpg'
-          },
-          {
-            title:'컨저링',
-            poster:'http://image.cine21.com/resize/cine21/poster/2013/0902/10_10_02__5223e56a44558[X230,330].jpg'
-          },
-          {
-            title:'도둑들',
-            poster:'http://image.cine21.com/resize/cine21/poster/2012/0629/09_53_58__4fecfca6a36e3[X230,330].jpg'
-          }
-      ]
-      })
-    },2000)
+  componentDidMount(){ 
+     
+      this._getMovies()
   }
   
   //랜더링 function
   _renderMovies =() =>{
     const movies=this.state.movies.map((x,index)=>{
-      return <Movie title={x.title} poster={x.poster} key={index}/>
+      return <Movie 
+      title={x.title} 
+      poster={x.medium_cover_image} 
+      key={index}
+      genres={x.genres}
+      synopsis={x.synopsis}/>
     });
     return movies;
   }
@@ -50,7 +59,13 @@ class App extends Component{
         //this.state.movies가 있을땐 _renderMovies 실행
         //state가 비어있는 상황일땐 loading...문구
         this.state.movies ? this._renderMovies() : 'Loading'
-        
+        /*
+        if(this.tate.movie){
+          this._renderMovies()
+        }else{
+          loading
+        }
+         */
       }
       </div>
     );
--- src/Movie.js
+++ src/Movie.js
@@ -1,33 +1,96 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
+import LinesEllipsis from 'react-lines-ellipsis'
 
-class Movie extends Component{
+/*
+class Movie extends Component...
+클래스형 컴포넌트
+장점: state를 쓸 수 있음
+    -componentDidMount(),
+    -componentWillMount()...
+단점: 코드가 복잡해짐
 
-    static propTypes={
-        title:PropTypes.string.isRequired,
-        poster:PropTypes.string.isRequired
-    }
-    render(){
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-            )
-    }//render
-
+function Movie({poster1, poster2...})
+함수형 컴포넌트(dumb component)
+장점:코드가 단순함
+단점:state가 없음
+ */
+function Movie({title, poster, genres, synopsis}){
+    return(
+        <div>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
+            <p>
+                {genres.map((y,index)=><MovieGenres genres={y} key={index}/>)}
+            </p>
+            <p>
+            <LinesEllipsis
+  text={synopsis}
+  maxLine='3'
+  ellipsis='...'
+  trimRight
+  basedOn='letters'
+/>
+            </p>
+        </div>
+    )
 }
-class MoviePoster extends Component{
+
+function MovieGenres({genres}){
+    return(
+        <span>{genres}</span>
+    )
+}
+// class Movie extends Component{
+
+//     static propTypes={
+//         title:PropTypes.string.isRequired,
+//         poster:PropTypes.string.isRequired
+//     }
+//     render(){
+//         return (
+//             <div>
+//                 <MoviePoster poster={this.props.poster}/>
+//                 <h1>{this.props.title}</h1>
+//             </div>
+//             )
+//     }//render
+
+// }
+/*
+component
+-props : 부모로부터 받아오는 값(읽기전용)
+-state: 각각의 컴포넌트가 가지는 객체
+   state에 변경사항이 있을 때 마다
+   component가 새로 랜더링 되기 때무내!!
+ */
+
+ function MoviePoster({poster}){
+     return(
+         <img src={poster} alt="Movie Poster"/>
+     )
+ }
+
+// class MoviePoster extends Component{
 
     
-    static propTypes={
-        poster:PropTypes.string.isRequired
-    }
+//     static propTypes={
+//         poster:PropTypes.string.isRequired
+//     }
 
-    render(){
-      return <img src={this.props.poster}/>
-    }
+//     render(){
+//       return <img src={this.props.poster}/>
+//     }
   
-  }
+//   }
+
+Movie.propTypes={
+    title:PropTypes.string.isRequired,
+    poster:PropTypes.string.isRequired
+}
+MoviePoster.propTypes={
+    poster:PropTypes.string.isRequired
+}
+
 export default Movie;
